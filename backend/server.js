1d8b4cebb35782606febfc5b747c441c6cbaa961
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-04-15 23:07:57 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 const orderRoutes = require("./routes/orderRoutes");
 app.use("/api", orderRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
